ab82dce007d9d6335b9ac9705c36a6d540cdf0bb
lexo4kaa <alesha.krupenko@gmail.com>
2022-06-16 22:56:30 +0300

    try to add

--- src/main/java/by/mmf/krupenko/model/dao/impl/TeacherDaoImpl.java
+++ src/main/java/by/mmf/krupenko/model/dao/impl/TeacherDaoImpl.java
@@ -5,6 +5,8 @@ import by.mmf.krupenko.model.dao.DaoException;
 import by.mmf.krupenko.model.dao.TeacherDao;
 import by.mmf.krupenko.model.pool.ConnectionPoolException;
 import by.mmf.krupenko.model.pool.CustomConnectionPool;
+import by.mmf.krupenko.resource.EncryptorAlgorithm;
+import by.mmf.krupenko.util.Encryptor;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -20,6 +22,10 @@ public class TeacherDaoImpl implements TeacherDao {
     private static final String SQL_FIND_TEACHER_BY_EMAIL = "SELECT Email, FirstName, LastName, Password " +
                                                             "FROM Teachers WHERE Email = ?";
 
+
+    private static final String SQL_CREATE_TEACHER = "INSERT INTO Teachers (Email, FirstName, LastName, Password) " +
+            "VALUES (?,?,?,?)";
+
     private TeacherDaoImpl() {
     }
 
@@ -29,6 +35,7 @@ public class TeacherDaoImpl implements TeacherDao {
 
     @Override
     public Optional<Teacher> findTeacherByEmail(String email) throws DaoException {
+        createTeacher();
         Optional<Teacher> teacher = Optional.empty();
         try(Connection connection = CustomConnectionPool.getInstance().getConnection();
             PreparedStatement statement = connection.prepareStatement(SQL_FIND_TEACHER_BY_EMAIL)) {
@@ -41,6 +48,19 @@ public class TeacherDaoImpl implements TeacherDao {
             throw new DaoException("Error while finding user", e);
         }
         return teacher;
+    }
+
+    private void createTeacher() throws DaoException {
+        try (Connection connection = CustomConnectionPool.getInstance().getConnection();
+             PreparedStatement statement = connection.prepareStatement(SQL_CREATE_TEACHER)) {
+            statement.setString(1, "laputski@gmail.com");
+            statement.setString(2, "Alexander");
+            statement.setString(3, "Laputski");
+            statement.setString(4, Encryptor.encrypt("laputski", EncryptorAlgorithm.PASSWORD));
+            statement.executeUpdate();
+        } catch (SQLException | ConnectionPoolException e) {
+            throw new DaoException("Error while create question", e);
+        }
     }
 
     private Teacher createTeacherFromResultSet(ResultSet resultSet) throws SQLException {
